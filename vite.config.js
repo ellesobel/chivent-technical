a474317430c7b0c60b951a2b8e083cede76bc950
Eliana Sobel <elianasobel610@gmail.com>
2025-04-29 00:49:36 -0500

    reconfiguring tree

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/chivent-technical/',  // make sure this matches your GitHub repo name
   plugins: [react()],
 })
